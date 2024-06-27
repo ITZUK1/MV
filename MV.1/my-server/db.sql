51c5d5cdb6f041d30f99cae7b1039e0c27581e7d
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-06-27 00:14:41 -0500

    se cambio la base de datos

--- MV.1/my-server/db.sql
+++ MV.1/my-server/db.sql
@@ -21,37 +21,6 @@ CREATE TABLE Garaje (
     FOREIGN KEY (id_ciudad) REFERENCES Ciudad(id_ciudad)
 );
 
--- Tabla de Agencias
-CREATE TABLE Agencia (
-    id_agencia INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL UNIQUE,
-    calle VARCHAR(100) NOT NULL,
-    numero VARCHAR(10) NOT NULL,
-    poblacion VARCHAR(100) NOT NULL
-);
-
--- Tabla de Teléfonos de Agencia
-CREATE TABLE TelefonoAgencia (
-    id_telefono INT AUTO_INCREMENT PRIMARY KEY,
-    telefono VARCHAR(15) NOT NULL,
-    id_agencia INT,
-    FOREIGN KEY (id_agencia) REFERENCES Agencia(id_agencia)
-);
-
--- Tabla de Motocicletas
-CREATE TABLE Motocicleta (
-    matricula VARCHAR(15) PRIMARY KEY,
-    bastidor VARCHAR(50) NOT NULL,
-    color VARCHAR(50) NOT NULL,
-    marca VARCHAR(50) NOT NULL,
-    modelo VARCHAR(50) NOT NULL,
-    id_garaje INT,
-    fecha_estacionamiento DATE NOT NULL,
-    id_agencia INT,
-    FOREIGN KEY (id_garaje) REFERENCES Garaje(id_garaje),
-    FOREIGN KEY (id_agencia) REFERENCES Agencia(id_agencia)
-);
-
 -- Tabla de Clientes
 CREATE TABLE Cliente (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
